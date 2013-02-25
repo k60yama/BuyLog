47471302ae07496189f96d1df4be71e17a14107d
ict <ict@R1-04.local>
2013-02-25 20:57:23 +0900

    update layout

--- gen/com/android/buylog/R.java
+++ gen/com/android/buylog/R.java
@@ -14,6 +14,7 @@ public final class R {
         public static final int burlywood=0x7f040001;
         public static final int peru=0x7f040000;
         public static final int sandybrown=0x7f040002;
+        public static final int textbg=0x7f040003;
     }
     public static final class dimen {
         public static final int padding_large=0x7f050002;
@@ -29,54 +30,71 @@ public final class R {
         public static final int createlog_close=0x7f020005;
         public static final int createlog_close_pressed=0x7f020006;
         public static final int createlog_close_stateful=0x7f020007;
-        public static final int createlog_pencil=0x7f020008;
-        public static final int createlog_pencil_off=0x7f020009;
-        public static final int createlog_pencil_on=0x7f02000a;
-        public static final int createlog_question=0x7f02000b;
-        public static final int createlog_question_pressed=0x7f02000c;
-        public static final int createlog_question_stateful=0x7f02000d;
-        public static final int cursor_focused=0x7f02000e;
-        public static final int cursor_off=0x7f02000f;
-        public static final int cursor_pressed=0x7f020010;
-        public static final int ic_action_search=0x7f020011;
-        public static final int icon=0x7f020012;
-        public static final int mainmenu_stateful=0x7f020013;
-        public static final int shop=0x7f020014;
+        public static final int createlog_line=0x7f020008;
+        public static final int createlog_pencil=0x7f020009;
+        public static final int createlog_pencil_off=0x7f02000a;
+        public static final int createlog_pencil_on=0x7f02000b;
+        public static final int createlog_question=0x7f02000c;
+        public static final int createlog_question_pressed=0x7f02000d;
+        public static final int createlog_question_stateful=0x7f02000e;
+        public static final int cursor_focused=0x7f02000f;
+        public static final int cursor_off=0x7f020010;
+        public static final int cursor_pressed=0x7f020011;
+        public static final int ic_action_search=0x7f020012;
+        public static final int icon=0x7f020013;
+        public static final int mainmenu_stateful=0x7f020014;
+        public static final int shop=0x7f020015;
     }
     public static final class id {
         public static final int function_about=0x7f090001;
         public static final int function_name=0x7f090000;
-        public static final int menu_settings=0x7f090007;
-        public static final int moveNewLog=0x7f090003;
-        public static final int moveShowLog=0x7f090005;
-        public static final int newLog=0x7f090004;
-        public static final int showLog=0x7f090006;
-        public static final int title=0x7f090002;
+        public static final int itemTitle=0x7f090002;
+        public static final int menu_settings=0x7f09000b;
+        public static final int moveNewLog=0x7f090007;
+        public static final int moveShowLog=0x7f090009;
+        public static final int newLog=0x7f090008;
+        public static final int pictureTitle=0x7f090005;
+        public static final int priceTitle=0x7f090003;
+        public static final int showLog=0x7f09000a;
+        public static final int storeTitle=0x7f090004;
+        public static final int title=0x7f090006;
     }
     public static final class layout {
         public static final int about_this_createlog=0x7f030000;
         public static final int about_this_function=0x7f030001;
-        public static final int create_log=0x7f030002;
-        public static final int create_log_title=0x7f030003;
-        public static final int function_new_log=0x7f030004;
-        public static final int function_show_log=0x7f030005;
-        public static final int main_menu=0x7f030006;
+        public static final int createlog=0x7f030002;
+        public static final int createlog_item_title=0x7f030003;
+        public static final int createlog_line=0x7f030004;
+        public static final int createlog_picture_title=0x7f030005;
+        public static final int createlog_price_title=0x7f030006;
+        public static final int createlog_store_title=0x7f030007;
+        public static final int createlog_title=0x7f030008;
+        public static final int function_new_log=0x7f030009;
+        public static final int function_show_log=0x7f03000a;
+        public static final int main_menu=0x7f03000b;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
+        public static final int confirm=0x7f06000f;
         public static final int createLogTitle=0x7f06000a;
         public static final int create_log_about=0x7f060008;
         public static final int hello_world=0x7f060001;
+        /**  create_log.xml 
+         */
+        public static final int itemTitle=0x7f06000b;
         public static final int menu_settings=0x7f060002;
         /**  about_this_function.xml 
          */
         public static final int new_log=0x7f060004;
         public static final int new_log_about=0x7f060006;
+        public static final int pictureTitle=0x7f06000e;
+        public static final int priceTitle=0x7f06000c;
         public static final int show_log=0x7f060005;
         public static final int show_log_about=0x7f060007;
+        public static final int storeTitle=0x7f06000d;
         public static final int title_activity_main=0x7f060003;
         /**  create_log_title.xml 
          */
--- src/com/android/buylog/CreateLogActivity.java
+++ src/com/android/buylog/CreateLogActivity.java
@@ -7,8 +7,10 @@ import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 
-public class CreateLogActivity extends Activity {
+public class CreateLogActivity extends Activity{
 
+	private Typeface tf;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		//ActivityクラスのonCreateを実行
@@ -18,33 +20,55 @@ public class CreateLogActivity extends Activity {
 		this.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		
 		//レイアウト設定ファイルを指定
-		this.setContentView(R.layout.create_log);
+		this.setContentView(R.layout.createlog);
 		
-		//タイトルセットアップ
-		this.setTitle();
-		
-		//フッター初期設定
-		String title = this.getString(R.string.createLogTitle);
-		String about = this.getString(R.string.create_log_about);
-		this.setFooter(title, about);
+		//レイアウトセットアップ
+		this.setUp();
 	}
+	
 	
 	public void finishActivity(View view){
 		//アクティビティ終了
 		this.finish();
 	}
 	
+	
+	private void setUp(){
+		//タイトルセットアップ
+		this.setTitle();
+		
+		//TextViewインスタンス取得
+		TextView[] itemTitles = {
+				(TextView)this.findViewById(R.id.itemTitle),
+				(TextView)this.findViewById(R.id.priceTitle),
+				(TextView)this.findViewById(R.id.storeTitle),
+				(TextView)this.findViewById(R.id.pictureTitle)
+		};
+		
+		//フォント設定
+		for(TextView tv : itemTitles){
+			tv.setTypeface(tf);
+		}
+		
+		//フッター初期設定
+		String title = this.getString(R.string.createLogTitle);
+		String about = this.getString(R.string.create_log_about);
+		this.setFooter(title, about);		
+	}
+	
+	
 	private void setTitle(){
 		//カスタムタイトルバーを使用
-		this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.create_log_title);
+		this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.createlog_title);
 		
 		//フォントファイル取得
-		Typeface tf = Typeface.createFromAsset(this.getAssets(), "fonts/APJapanesefont.ttf");
+		tf = Typeface.createFromAsset(this.getAssets(), "fonts/APJapanesefont.ttf");
 		
 		//TextViewインスタンスを取得
 		TextView tv = (TextView)this.findViewById(R.id.title);
 		tv.setTypeface(tf);
 	}
+	
 	
 	private void setFooter(String aboutTitle, String aboutMsg){
 		//TextViewインスタンス取得
@@ -57,5 +81,55 @@ public class CreateLogActivity extends Activity {
 		//機能説明
 		tv = (TextView)this.findViewById(R.id.function_about);
 		tv.setText(aboutMsg);
+	}
+	
+	
+	//商品名を押下した場合の処理
+	private int itemTitleStatus = 0;	//展開省略フラグ
+	public void itemTitle(View view){		
+		//アイコン変更処理へ
+		//処理結果を格納
+		this.itemTitleStatus = this.titleIconChange((TextView)view, this.itemTitleStatus);
+	}
+	
+	//値段を押下した場合の処理
+	private int priceTitleStatus = 0;	//展開省略フラグ
+	public void priceTitle(View view){		
+		//アイコン変更処理へ
+		//処理結果を格納
+		this.priceTitleStatus = this.titleIconChange((TextView)view, this.priceTitleStatus);
+	}	
+
+	//ショップ情報を押下した場合の処理
+	private int storeTitleStatus = 0;	//展開省略フラグ
+	public void storeTitle(View view){		
+		//アイコン変更処理へ
+		//処理結果を格納
+		this.storeTitleStatus = this.titleIconChange((TextView)view, this.storeTitleStatus);
+	}	
+	
+	//商品イメージを押下した場合の処理
+	private int pictureTitleStatus = 0;	//展開省略フラグ
+	public void pictureTitle(View view){		
+		//アイコン変更処理へ
+		//処理結果を格納
+		this.pictureTitleStatus = this.titleIconChange((TextView)view, this.pictureTitleStatus);
+	}
+	
+	//アイコン変更処理
+	public int titleIconChange(TextView view, int iconStatus){
+		int drawableId;		//アイコンID
+		if(iconStatus == 0){
+			//展開用アイコン設定
+			drawableId = R.drawable.createlog_pencil_on;
+			iconStatus = 1;
+		}else{
+			//省略用アイコン設定
+			drawableId = R.drawable.createlog_pencil_off;
+			iconStatus = 0;
+		}
+		//アイコン設定
+		view.setCompoundDrawablesWithIntrinsicBounds(drawableId, 0, 0, 0);
+		return iconStatus;
 	}
 }
